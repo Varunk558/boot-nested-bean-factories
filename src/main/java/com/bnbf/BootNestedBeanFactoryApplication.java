8eb0369b37ab6bb3efa094ee346a65f55f95c814
Varun Chowdary <varun@Varuns-MacBook-Pro.local>
2025-05-02 17:28:52 -0500

    Springboot nested bean factory application

---
+++ src/main/java/com/bnbf/BootNestedBeanFactoryApplication.java
@@ -0,0 +1,14 @@
+package com.bnbf;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class BootNestedBeanFactoryApplication {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/com/bnbf/beans/Robot.java
@@ -0,0 +1,4 @@
+package com.bnbf.beans;
+
+public class Robot {
+}
